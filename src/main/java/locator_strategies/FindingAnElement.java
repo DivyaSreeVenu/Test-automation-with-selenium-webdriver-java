11a563ef99b6f5cba1de9fbde4aef55b61c54507
N V Sai Divyasree <saidivyasree.nv.97@gmail.com>
2025-11-08 21:32:31 +0000

    Webelement locator

--- src/main/java/locator_strategies/FindingAnElement.java
+++ src/main/java/locator_strategies/FindingAnElement.java
@@ -1,5 +1,7 @@
 package locator_strategies;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -9,8 +11,10 @@ public class FindingAnElement extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
-
-    
+    WebElement emailtb = driver.findElement(By.id("email"));
+    emailtb.sendKeys("test@1");
+    List<WebElement> radiobuttons = driver.findElements(By.cssSelector("input[type='radio']"));
+    System.out.println("Number of radio buttons:" + radiobuttons.size());
 
     driver.quit();
   }
