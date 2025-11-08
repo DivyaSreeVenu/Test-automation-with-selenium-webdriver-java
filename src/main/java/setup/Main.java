f21e748f885deab4251c3e5c5724fb534e091852
N V Sai Divyasree <saidivyasree.nv.97@gmail.com>
2025-11-08 20:43:10 +0000

    First commit

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -7,13 +7,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class Main {
-
-
+  public static WebDriver driver;
   static {
-
+    chromedriver().setup();
+    var options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    driver = new ChromeDriver(options);
   }
 
   public static void main(String[] args) {
-
+    driver.get("https://google.com");
+    driver.quit();
   }
 }
