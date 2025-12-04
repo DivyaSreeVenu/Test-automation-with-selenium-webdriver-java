f5f20f8578c5fc819b6efd642d177ea9838fd937
N V Sai Divyasree <saidivyasree.nv.97@gmail.com>
2025-12-04 23:51:09 +0000

    Challenge

--- src/main/java/locator_strategies/Challenge_FindElements.java
+++ src/main/java/locator_strategies/Challenge_FindElements.java
@@ -10,9 +10,9 @@ public class Challenge_FindElements extends Main {
   @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
-
-    // TODO: add your solution here
-
+    WebElement imageButton = driver.findElement(By.id("imageButton"));
+    WebElement input = driver.findElement(By.cssSelector("input[value='Click']"));
+    WebElement nameTxtbox = driver.findElement(By.name("id-name2"));
 
     driver.quit();
   }
